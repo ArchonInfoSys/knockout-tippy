203cba637b470781a9a7612e24e25ffd2f8e1dfe
Mark D'Arensbourg <mdarens@gmail.com>
2016-05-20 11:28:41 -0500

    Drop string template engine (#1)
    * Assume we already have stringTemplateEngine
    
    * breaking change

--- knockout.tippy.js
+++ knockout.tippy.js
@@ -1,7 +1,7 @@
 (function(root, factory) {
 	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
-		define(["jquery", "knockout", "stringTemplateEngine"], factory);
+		define(["jquery", "knockout"], factory);
 	} else {
 		// Browser globals
 		factory($, ko);
