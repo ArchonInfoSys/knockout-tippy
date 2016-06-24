17f3991983ef429398f4103918a4df842335b176
Mark D'Arensbourg <mdarens@gmail.com>
2016-06-24 10:32:09 -0500

    make consumable via npm
    - drop jQuery dependency
    - include styles

--- knockout.tippy.js
+++ knockout.tippy.js
@@ -1,31 +1,24 @@
-(function(root, factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(["jquery", "knockout"], factory);
-	} else {
-		// Browser globals
-		factory($, ko);
+var ko = require("knockout");
+require("css!less!./tippy.less");
+
+ko.bindingHandlers.tippy = {
+	init: function () {
+		return { controlsDescendantBindings: true };
+	},
+	update: function (element, valueAccessor, allBindingsAccessor, model) {
+		var name = ko.utils.unwrapObservable(valueAccessor());
+
+		ko.templates[name] = element.innerHTML;
+		ko.renderTemplate("knockout-tippy-main", {
+			name: name,
+			model: model
+		}, null, element, "replaceNode");
 	}
-}(this, function ($, ko) {
-	ko.bindingHandlers.tippy = {
-		init: function (element) {
-			return { controlsDescendantBindings: true };
-		},
-		update: function (element, valueAccessor, allBindingsAccessor, model) {
-			var name = ko.utils.unwrapObservable(valueAccessor());
+};
 
-			ko.templates[name] = $(element).html();
-			ko.renderTemplate("knockout-tippy-main", {
-				name: name,
-				model: model
-			}, null, element, "replaceNode");
-		}
-	};
-
-	ko.templates["knockout-tippy-main"] =
-		'<div class="tippy" tabindex="0">' +
-			'<i class="tippy__hint__indicator icon-question-sign icon-large"></i>' +
-			'<div class="tippy__content" data-bind="template: { name: name, data: model }">' +
-			'</div>' +
-		'</div>';
-}));
+ko.templates["knockout-tippy-main"] =
+	"<div class=\"tippy\" tabindex=\"0\">" +
+		"<i class=\"tippy__hint__indicator icon-question-sign icon-large\"></i>" +
+		"<div class=\"tippy__content\" data-bind=\"template: { name: name, data: model }\">" +
+		"</div>" +
+	"</div>";
